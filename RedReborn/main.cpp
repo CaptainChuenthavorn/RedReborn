998f886ffe084fac8656c9b6fb09dab06fd764f5
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-10-30 15:46:31 +0700

    backup menu

--- RedReborn/main.cpp
+++ RedReborn/main.cpp
@@ -1,29 +1,57 @@
-Game::Game()
-	: mWindow(sf::VideoMode(640, 480), "SFML Application")
-	, mPlayer()
+#include "SFML/Window.hpp"
+#include <iostream>
+#include "Menu.h"
+int main()
 {
-	mPlayer.setRadius(40.f);
-	mPlayer.setPosition(100.f, 100.f);
-	mPlayer.setFillColor(sf::Color::Cyan);
-}
-void Game::run()
-{
-	while (mWindow.isOpen())
-	{
-		processEvents();
-		update();
-		render();
-	}
-}
-void Game::processEvents()
-{
-	sf::Event event;
-	while (mWindow.pollEvent(event))
-	{
-		if (event.type == sf::Event::Closed)
-			mWindow.close();
-	}
-}
-void Game::update()
-{
+    sf::RenderWindow window(sf::VideoMode(1080, 720), "My window");
+  
+    Menu menu(window.getSize().x,window.getSize().y);
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+            case sf::Event::KeyReleased:
+                switch (event.key.code) {
+                case sf::Keyboard::Up:
+                    menu.MoveUp();
+                    break;
+
+                case sf::Keyboard::Down:
+                    menu.MoveDown();
+                    break;
+                case sf::Keyboard::Return:
+                    switch (menu.GetPressedItem()) {
+                    case 0:
+                        std::cout << "Play has been pressed" << std::endl;
+                        break;
+
+                    case 1:
+                        std::cout << "Option has been pressed" << std::endl;
+                        break;
+
+                    case 2:
+                        std::cout << "Exit has been pressed" << std::endl;
+                        break;
+                    }
+                    break;
+                }
+    
+                     break;
+                case sf::Event::Closed:
+                window.close();
+                break; 
+            }
+            
+        }
+        window.clear();
+        menu.draw(window);
+        window.display();
+    }
+
+
+    return 0;
 }
