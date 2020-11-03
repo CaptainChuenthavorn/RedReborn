17f1aad61969db010334cf0f64d411fef74c6905
จักรภัทรณ์ ชื่นถาวร <captainchuenthavorn@hotmail.com>
2020-11-03 11:48:04 +0700

    backUp Chapter 5

--- RedReborn/Game.cpp
+++ RedReborn/Game.cpp
@@ -29,15 +29,13 @@ void Game::MoveMushroom() {
 	sf::Vector2u l_windSize = m_window.GetWindowSize();
 	sf::Vector2u l_textSize = m_mushroomTexture.getSize();
 
-	if ((m_mushroom.getPosition().x >
-		l_windSize.x - l_textSize.x && m_increment.x > 0) ||
-		(m_mushroom.getPosition().x < 0 && m_increment.x < 0)) {
+	if ((m_mushroom.getPosition().x >l_windSize.x - l_textSize.x && m_increment.x > 0) ||(m_mushroom.getPosition().x < 0 && m_increment.x < 0)) 
+	{
 		m_increment.x = -m_increment.x;
 	}
 
-	if ((m_mushroom.getPosition().y >
-		l_windSize.y - l_textSize.y && m_increment.y > 0) ||
-		(m_mushroom.getPosition().y < 0 && m_increment.y < 0)) {
+	if ((m_mushroom.getPosition().y >l_windSize.y - l_textSize.y && m_increment.y > 0) ||(m_mushroom.getPosition().y < 0 && m_increment.y < 0)) 
+	{
 		m_increment.y = -m_increment.y;
 	}
 
